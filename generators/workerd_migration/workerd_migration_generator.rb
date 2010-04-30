24c6611fd6a0ad0404b5bdd506015257ff11f896
Marius Nuennerich <marius@nuenneri.ch>
2010-04-30 12:32:43 +0200

    make migration generator work with rails 3

--- generators/workerd_migration/workerd_migration_generator.rb
+++
@@ -1,9 +0,0 @@
-class WorkerdMigrationGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.migration_template 'migration_template.rb', 'db/migrate', :assigns => {
-        :migration_name => "CreateWorkpieces"
-      }, :migration_file_name => "create_workpieces"
-    end
-  end
-end
